8a16c1e6ce7888a44a106740715561ef007900fd
Pixie <pixiestudio@gmail.com>
2020-12-17 13:33:38 +0100

    clean up dotenv

--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports.DB_FILE_NAME = './database.txt'
-module.exports.API_BASE = 'https://api.miro.com/v1'
-module.exports.BASE_URL = process.env.BASE_URL
-module.exports.CLIENT_ID = process.env.CLIENT_ID
-module.exports.CLIENT_SECRET = process.env.CLIENT_SECRET
-module.exports.WEBHOOKS_VERIFICATION_TOKEN = process.env.WEBHOOKS_VERIFICATION_TOKEN
